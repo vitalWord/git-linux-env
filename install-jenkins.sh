f6eb8463b3b615e1b6788348dc7cbf3993d92aad
Vitalii Lysenko <vitalWord@gmail.com>
2013-11-04 14:13:34 -0800

    Add jenkins install script.

---
+++ install-jenkins.sh
@@ -0,0 +1,8 @@
+sudo yum -y install java
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum -y install jenkins
+
+sudo service jenkins start
+
